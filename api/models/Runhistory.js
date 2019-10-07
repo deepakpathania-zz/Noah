84b2d3b9aa812c0e0b613d4b92edf8132460f108
deepakpathania <deepakpathania789@gmail.com>
2019-10-07 11:49:16 +0530

    :construction: Added cron for processing scheduled tasks

---
+++ api/models/Runhistory.js
@@ -0,0 +1,19 @@
+module.exports = {
+  schema: true,
+  primaryKey: 'id',
+  attributes: {
+    id: {
+      type: 'number',
+      autoIncrement: true
+    },
+    schedule: {
+      model: 'schedule'
+    },
+    runTime: {
+      type: 'number'
+    },
+    responseStatusCode: {
+      type: 'number'
+    }
+  }
+};
---
+++ api/services/CronService.js
@@ -0,0 +1,97 @@
+module.exports = {
+  process: (cb) => {
+    async.waterfall([
+      /**
+       * Filter out the schedules that need to be run
+       * based on `nextRunningTime` and `status`.
+       */
+      (next) => {
+        Schedule
+          .find({
+            nextRunningTime: {
+              '<=': Date.now()
+            },
+            status: 1
+          })
+          .exec((err, schedules) => {
+            return next(err, schedules);
+          });
+      },
+      /**
+       * Make the corresponding request for each eligible schedule.
+       * Bail out early in case of no eligible schedules.
+       */
+      (schedules, next) => {
+        if (schedules.length === 0) {
+          return next(new Error('noSchedulesToProcessError'));
+        }
+
+        async.map(schedules, async.reflect((schedule, callback) => {
+          const { request, period } = schedule;
+
+          async.waterfall([
+            /**
+             * Make the request for the schedule.
+             */
+            (cbi) => {
+              UtilService.makeRequest(request, (err, response) => {
+                return cbi(err, _.get(response, 'statusCode'), Date.now());
+              });
+            },
+            /**
+             * Update the `nextRunningTime` based on period.
+             */
+            (responseStatusCode, runTime, cbi) => {
+              const nextRunningTime = UtilService.getNextRunningTime(period);
+
+              Schedule
+                .update({
+                  id: schedule.id
+                })
+                .set({
+                  nextRunningTime
+                })
+                .exec((err) => {
+                  return cbi(err, responseStatusCode, runTime);
+                });
+            },
+            /**
+             * Create a run history record for this schedule.
+             */
+            (responseStatusCode, runTime, cbi) => {
+              Runhistory
+                .create({
+                  schedule: schedule.id,
+                  runTime: runTime,
+                  responseStatusCode: responseStatusCode
+                })
+                .exec((err) => {
+                  return cbi(err);
+                });
+            }
+          ], callback);
+        }), (err, results) => {
+          if (err) { return next(err); }
+
+          for (var i = 0; i < results.length; i++) {
+            if (results[i].error) {
+              sails.log.error(results[i].error);
+            }
+          }
+
+          return next(null);
+        });
+      }
+    ], (err) => {
+      if (err) {
+        if (err.message === 'noSchedulesToProcessError') {
+          return cb(null);
+        }
+
+        return cb(err);
+      }
+
+      return cb(null);
+    });
+  }
+};
